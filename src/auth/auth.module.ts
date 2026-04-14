faba7adeb757850ad8849792ef81f05b40fb7f73
Денис Kоверзнев <dionisk2006@gmail.com>
2026-04-14 09:56:55 +0000

    Time Auth Changed

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -17,7 +17,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: '1h' },
+        signOptions: { expiresIn: '2h' },
       }),
     }),
   ],
